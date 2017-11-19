e5369c66a753c19da5e6aa83c2400d7462cc274a
Jeff Bauer <SirGeoffers@users.noreply.github.com>
2017-11-19 16:24:28 -0500

    Added ability to record data to file while streaming from Kinect

--- Assets/Scripts/KinectSocketStream.cs
+++ Assets/Scripts/KinectSocketStream.cs
@@ -2,15 +2,19 @@
 using System.Net;
 using System.Net.Sockets;
 using System.Threading;
+using System.IO;
 using UnityEngine;
 
 public class KinectSocketStream : KinectStream {
   public string IP = "127.0.0.1";
   public int Port = 16000;
+  public string OutputFile;
 
   UdpClient udpClient;
   IPEndPoint ipEndPoint;
   Thread thread;
+
+  StreamWriter outputWriter;
 
   bool threadDone = false;
 
@@ -21,6 +25,8 @@ public class KinectSocketStream : KinectStream {
 
     JointData = new float[25*3];
 
+    outputWriter = new StreamWriter("Assets/Resources/" + OutputFile);
+
     thread = new Thread(new ThreadStart(ReadJointData));
     thread.IsBackground = true;
     thread.Start();
@@ -30,9 +36,14 @@ public class KinectSocketStream : KinectStream {
   void ReadJointData() {
     try {
       while (!threadDone) {
-                // Each coord is a 32 bit float
-                Byte[] buffer = udpClient.Receive(ref ipEndPoint);
-                Buffer.BlockCopy(buffer, 0, JointData, 0, buffer.Length);
+        // Each coord is a 32 bit float
+        Byte[] buffer = udpClient.Receive(ref ipEndPoint);
+        Buffer.BlockCopy(buffer, 0, JointData, 0, buffer.Length);
+        foreach (float val in JointData) {
+          outputWriter.Write(val.ToString());
+          outputWriter.Write(" ");
+        }
+        outputWriter.Write("\n");
       }
     } catch (Exception e) {
       Debug.Log("UDP Client Error: " + e);
